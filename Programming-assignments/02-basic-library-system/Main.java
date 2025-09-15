5a3db6c5f97b13573a71ab644119026042f3fd1d
Khaled Abbara <148062226+Khaled-Abbara@users.noreply.github.com>
2025-09-15 17:47:08 +0800

    Submit

--- Programming-assignments/02-basic-library-system/Main.java
+++ Programming-assignments/02-basic-library-system/Main.java
@@ -1,116 +1,116 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// public class Main {
-//     static Scanner scanner = new Scanner(System.in);
-//     static String title;
-//     static String author;
-//     static int quantity;
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    static String title;
+    static String author;
+    static int quantity;
 
-//     public static void main(String[] args) {
+    public static void main(String[] args) {
 
-//         Library libraryLog = new Library(100);
-//         Library library = new Library(100);
+        Library libraryLog = new Library(100);
+        Library library = new Library(100);
 
-//         library.addBook("The Hobbit", "J.R.R. Tolkien", 2);
-//         library.addBook("1984", "George Orwell", 1);
-//         library.addBook("To Kill", "Harper Lee", 3);
-//         library.addBook("The Catcher", "J.D. Salinger", 4);
-//         library.addBook("Harry Potter", "J.K. Rowling", 5);
-//         library.addBook("The Great Gatsby", "F. Scott Fitzgerald", 1);
-//         library.addBook("Moby Dick", "Herman Melville", 0);
-//         library.addBook("Pride and Prejudice", "Jane Austen", 2);
-//         library.addBook("The Lord of the Rings", "J.R.R. Tolkien", 1);
-//         library.addBook("Brave New World", "Aldous Huxley", 0);
+        library.addBook("The Hobbit", "J.R.R. Tolkien", 2);
+        library.addBook("1984", "George Orwell", 1);
+        library.addBook("To Kill", "Harper Lee", 3);
+        library.addBook("The Catcher", "J.D. Salinger", 4);
+        library.addBook("Harry Potter", "J.K. Rowling", 5);
+        library.addBook("The Great Gatsby", "F. Scott Fitzgerald", 1);
+        library.addBook("Moby Dick", "Herman Melville", 0);
+        library.addBook("Pride and Prejudice", "Jane Austen", 2);
+        library.addBook("The Lord of the Rings", "J.R.R. Tolkien", 1);
+        library.addBook("Brave New World", "Aldous Huxley", 0);
 
 
-//         int choice;
+        int choice;
 
-//         do {
-//             System.out.println("\n\n");
-//             System.out.println("======| Library Menu |======");
-//             System.out.println("1. View all books");
-//             System.out.println("2. Borrow a book");
-//             System.out.println("3. Add a book");
-//             System.out.println("4. Return a book");
-//             System.out.println("5. Exit");
-//             System.out.print("Enter your choice: ");
-//             choice = scanner.nextInt();
-//             scanner.nextLine(); // consume newline
+        do {
+            System.out.println("\n\n");
+            System.out.println("======| Library Menu |======");
+            System.out.println("1. View all books");
+            System.out.println("2. Borrow a book");
+            System.out.println("3. Add a book");
+            System.out.println("4. Return a book");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine(); // consume newline
 
-//             switch (choice) {
-//                 // view books
-//                 case 1:
-//                     library.viewAllBooks();
-//                     break;
+            switch (choice) {
+                // view books
+                case 1:
+                    library.viewAllBooks();
+                    break;
 
-//                 // borrow books
-//                 case 2:
-//                     getBookDetails();
+                // borrow books
+                case 2:
+                    getBookDetails();
 
-//                     // logic
-//                     library.borrowBook(title, quantity);
-//                     libraryLog.addBook(title, author, quantity);
+                    // logic
+                    library.borrowBook(title, quantity);
+                    libraryLog.addBook(title, author, quantity);
 
-//                     // clear variables
-//                     clearVariables();
-//                     break;
+                    // clear variables
+                    clearVariables();
+                    break;
 
-//                 // add books
-//                 case 3:
-//                     getBookDetails();
+                // add books
+                case 3:
+                    getBookDetails();
 
-//                     // logic
-//                     library.addBook(title, author, quantity);
+                    // logic
+                    library.addBook(title, author, quantity);
 
-//                     // clear variables
-//                     clearVariables();
-//                     break;
+                    // clear variables
+                    clearVariables();
+                    break;
 
-//                 // Return a book
-//                 case 4:
-//                     getBookDetails();
+                // Return a book
+                case 4:
+                    getBookDetails();
 
-//                     // logic
-//                     if (libraryLog.doesExist(title, author, quantity)) {
+                    // logic
+                    if (libraryLog.doesExist(title, author, quantity)) {
 
-//                         library.returnBook(title, quantity);
-//                         libraryLog.returnBook(title, quantity);
-//                         System.out.println("Book Sucessfully returned!");
+                        library.returnBook(title, quantity);
+                        libraryLog.returnBook(title, quantity);
+                        System.out.println("Book Sucessfully returned!");
 
-//                     } else {
-//                         System.out.println("Incorrect book details, or Book does not belong to the library!");
+                    } else {
+                        System.out.println("Incorrect book details, or Book does not belong to the library!");
                         
-//                     }
+                    }
 
-//                     // clear variables
-//                     clearVariables();
-//                     break;
+                    // clear variables
+                    clearVariables();
+                    break;
 
-//                 default:
-//                     System.out.println("Invalid choice. Try again.");
-//                     break;
-//             }
-//         } while (choice != 5);
+                default:
+                    System.out.println("Invalid choice. Try again.");
+                    break;
+            }
+        } while (choice != 5);
 
-//         System.out.println("See you again!");
-//         scanner.close();
-//     }
+        System.out.println("See you again!");
+        scanner.close();
+    }
 
-//     static void getBookDetails() {
-//         System.out.println("Enter the book's title: ");
-//         title = scanner.nextLine();
-//         System.out.println("Enter the book's author: ");
-//         author = scanner.nextLine();
-//         System.out.println("Enter the quantity: ");
-//         quantity = scanner.nextInt();
-//     }
+    static void getBookDetails() {
+        System.out.println("Enter the book's title: ");
+        title = scanner.nextLine();
+        System.out.println("Enter the book's author: ");
+        author = scanner.nextLine();
+        System.out.println("Enter the quantity: ");
+        quantity = scanner.nextInt();
+    }
 
-//     static void clearVariables() {
-//         title = "";
-//         author = "";
-//         quantity = 0;
-//     }
-// }
+    static void clearVariables() {
+        title = "";
+        author = "";
+        quantity = 0;
+    }
+}
 
 
 class Library {
@@ -124,6 +124,9 @@ class Library {
 
     // specifed methods
     public void addBook(String title, String author, int quantity) {
+        
+        // We are adding both in the logs and the books
+
         for (int i = 0; i < books.length; i++) {
             if (books[i] == null) {
                 books[i] = new Book(title, author, quantity);
@@ -133,9 +136,20 @@ class Library {
                 books[i].increaseQuantity(quantity);
             }
         }
+
+        for (int i = 0; i < records.length; i++) {
+            if (records[i] == null) {
+                records[i] = new Book(title, author, quantity);
+                break;
+
+            } else {
+                records[i].increaseQuantity(quantity);
+            }
+        }
     }
 
     public void borrowBook(String title, String author, int quantity) {
+
         Book targetBook = null;
 
         for (Book book : books) {
@@ -157,11 +171,24 @@ class Library {
             System.out.println("0");
         }
 
-        // excute logic
+        // =======================================================================================
+
+        Book loggedBook = null;
+
+        for (Book book : records) {
+            if (book.title.equalsIgnoreCase(title) && book.author.equalsIgnoreCase(author)) {
+                loggedBook = book;
+                break;
+            }
+        }
+
+        // logic
+        targetBook.decreaseQuantity(quantity);
+        loggedBook.increaseQuantity(quantity);
     }
 
-    public void returnBook(String title, String author, int amount) {
-        // Check if book even exists
+    public void returnBook(String title, String author, int quantity) {
+
         Book targetBook = null;
 
         for (Book book : books) {
@@ -175,40 +202,44 @@ class Library {
             System.out.println("0");
         }
 
-        // check if the book is present in the logs
+        if (targetBook.quantity <= 0) {
+            System.out.println("0");
+        }
+
+        // =======================================================================================
+
         Book loggedBook = null;
 
         for (Book book : records) {
             if (book.title.equalsIgnoreCase(title) && book.author.equalsIgnoreCase(author)) {
-                loggedBook = null;
+                loggedBook = book;
                 break;
             }
-        }
-
-        if (loggedBook == null) {
-            System.out.println("0");
         }
 
         if (loggedBook.quantity > targetBook.quantity) {
             System.out.println("0");
         }
 
-        // excute logic
+        // logic
+        targetBook.increaseQuantity(quantity);
+        loggedBook.decreaseQuantity(quantity);
+
     }
+
 
     // utility methods
-    public boolean doesExist(String title, String author, int quantity) {
-        for (Book book : books) {
-            if (book.title.equals(title) && book.author.equals(author) && book.quantity == quantity) {
-                return true;
-
-            }
-        }
-        return false;
-    }
 
     public void viewAllBooks() {
         for (Book book : books) {
+            if (book != null) {
+                System.out.println(book.toString());
+            }
+        }
+    }
+
+    public void viewAllLogs() {
+        for (Book book : records) {
             if (book != null) {
                 System.out.println(book.toString());
             }
