abbb7868fb12d7bf0283dce90a8e982d83097b9b
Khaled Abbara <148062226+Khaled-Abbara@users.noreply.github.com>
2025-10-02 01:25:25 +0800

    Submit

--- Programming-assignments/04-stock-price-data-analysis/Main.java
+++ Programming-assignments/04-stock-price-data-analysis/Main.java
@@ -1,3 +1,16 @@
 public class Main {
+    double[] stockPrices = {100.5, 102.3, 98.7, 105.2, 101.0, 99.8, 104.6, 103.1, 100.0, 102.5};
+
+
+    public static double calculateAveragePrice(double[] prices) {
+        double total = 0;
+
+        for (int index = 0; index < prices.length; index++) {
+            total += prices[index];
+        }
+
+        return (total / prices.length);
+    }  
+    
     
 }
