de33777ee53df60aa2716e94df0d4ce742d54ff7
Khaled Abbara <148062226+Khaled-Abbara@users.noreply.github.com>
2025-10-02 02:45:02 +0800

    Submit

--- Programming-assignments/04-stock-price-data-analysis/Main.java
+++ Programming-assignments/04-stock-price-data-analysis/Main.java
@@ -1,6 +1,17 @@
-public class Main {
-    double[] stockPrices = {100.5, 102.3, 98.7, 105.2, 101.0, 99.8, 104.6, 103.1, 100.0, 102.5};
+import java.util.ArrayList;
 
+public class Main {
+    public static void main(String[] args) {
+        double[] stockPrices = {100.5, 102.3, 98.7, 105.2, 101.0, 99.8, 104.6, 103.1, 100.0, 102.5};
+
+    System.out.println("Average Price: " + calculateAveragePrice(stockPrices));
+    System.out.println("Maximum Price: " + findMaximumPrice(stockPrices));
+    System.out.println("Occurrences of 102.5: " + countOccurrences(102.5, stockPrices));
+    System.out.println("Cumulative Prices: " + computeCumulativeSum(stockPrices));
+    
+    
+    // System.out.println("Minimum Price: " + findMinimumPrice(stockPrices));
+    }
 
     public static double calculateAveragePrice(double[] prices) {
         double total = 0;
@@ -24,7 +35,7 @@ public class Main {
         return maxPrice;
     }
 
-    public static int countOccurrences (int targetPrice, double[] prices) {
+    public static int countOccurrences (double targetPrice, double[] prices) {
         int counter = 0;
 
         for (int index = 0; index < prices.length; index++) {
@@ -34,5 +45,19 @@ public class Main {
         }
 
         return counter;
+    }
+
+    public static ArrayList<Double> computeCumulativeSum (double[] prices) {
+        
+        double currentValue = 0;
+        ArrayList<Double> cumulativePrices = new ArrayList<Double>();
+
+        for (int index = 0; index < prices.length; index++) {
+            currentValue += prices[index];
+            cumulativePrices.add(currentValue);
+        }
+
+        return cumulativePrices;
+
     }
 }
