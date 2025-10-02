d1c7b3c8386f6563c21f1769bfcbde99dfe3a97e
Khaled Abbara <148062226+Khaled-Abbara@users.noreply.github.com>
2025-10-02 14:00:39 +0800

    Submit

--- Programming-assignments/04-stock-price-data-analysis/Main.java
+++ Programming-assignments/04-stock-price-data-analysis/Main.java
@@ -2,15 +2,21 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        double[] stockPrices = {100.5, 102.3, 98.7, 105.2, 101.0, 99.8, 104.6, 103.1, 100.0, 102.5};
+        double[] stockPrices = { 100.5, 102.3, 98.7, 105.2, 101.0, 99.8, 104.6, 103.1, 100.0, 102.5 };
 
-    System.out.println("Average Price: " + calculateAveragePrice(stockPrices));
-    System.out.println("Maximum Price: " + findMaximumPrice(stockPrices));
-    System.out.println("Occurrences of 102.5: " + countOccurrences(102.5, stockPrices));
-    System.out.println("Cumulative Prices: " + computeCumulativeSum(stockPrices));
-    
-    
-    // System.out.println("Minimum Price: " + findMinimumPrice(stockPrices));
+        ArrayList<Double> stockPrices2 = new ArrayList<Double>();
+
+        for (double price : stockPrices) {
+            stockPrices2.add(price);
+        }
+
+        System.out.println("Average Price: " + calculateAveragePrice(stockPrices));
+        System.out.println("Maximum Price: " + findMaximumPrice(stockPrices));
+        System.out.println("Occurrences of 102.5: " + countOccurrences(102.5, stockPrices));
+        System.out.println("Cumulative Prices: " + computeCumulativeSum(stockPrices));
+        System.out.println("\n(Bonus)=======================================");
+        System.out.println("Minimum Price: " + findmMinimumPrice(stockPrices));
+        System.out.println("Daily Stock Changes: " + dailyChange(stockPrices));
     }
 
     public static double calculateAveragePrice(double[] prices) {
@@ -21,9 +27,9 @@ public class Main {
         }
 
         return (total / prices.length);
-    }  
-    
-    public static double findMaximumPrice (double[] prices) {
+    }
+
+    public static double findMaximumPrice(double[] prices) {
         double maxPrice = prices[0];
 
         for (int index = 1; index < prices.length; index++) {
@@ -35,7 +41,7 @@ public class Main {
         return maxPrice;
     }
 
-    public static int countOccurrences (double targetPrice, double[] prices) {
+    public static int countOccurrences(double targetPrice, double[] prices) {
         int counter = 0;
 
         for (int index = 0; index < prices.length; index++) {
@@ -47,8 +53,8 @@ public class Main {
         return counter;
     }
 
-    public static ArrayList<Double> computeCumulativeSum (double[] prices) {
-        
+    public static ArrayList<Double> computeCumulativeSum(double[] prices) {
+
         double currentValue = 0;
         ArrayList<Double> cumulativePrices = new ArrayList<Double>();
 
@@ -59,5 +65,32 @@ public class Main {
 
         return cumulativePrices;
 
+    }
+
+    // Bonus Methods
+
+    public static double findmMinimumPrice(double[] prices) {
+        double minPrice = prices[0];
+
+        for (int index = 1; index < prices.length; index++) {
+            if (minPrice > prices[index]) {
+                minPrice = prices[index];
+            }
+        }
+
+        return minPrice;
+    }
+
+    public static ArrayList<Double> dailyChange(double[] prices) {
+        ArrayList<Double> changes = new ArrayList<Double>();
+        double change;
+
+        for (int index = 0; index < prices.length - 1; index++) {
+            change = prices[index + 1] - prices[index];
+            change = Math.round(change * 100.0) / 100.0;
+            changes.add(change);
+        }
+
+        return changes;
     }
 }
