28dc6d817a58dbc3b08b9bf0f069e19d2dcf6c47
Khaled Abbara <148062226+Khaled-Abbara@users.noreply.github.com>
2025-10-04 11:31:46 +0800

    Submit

--- Programming-assignments/05-university-grade-and-enrollment-system/Main.java
+++ Programming-assignments/05-university-grade-and-enrollment-system/Main.java
@@ -1,3 +1,18 @@
 public class Main {
     
 }
+
+
+class Student {
+    int id;
+    String name;
+    String[] enrolledCourses;
+
+    public Student(int id, String name, String[] enrolledCourses) {
+        this.id = id;
+        this.name = name;
+        this.enrolledCourses = enrolledCourses;
+    }
+    
+}
+
