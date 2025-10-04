09047d94e91a059547883e3c3a74da973b721309
Khaled Abbara <148062226+Khaled-Abbara@users.noreply.github.com>
2025-10-04 12:17:59 +0800

    Submit

--- Programming-assignments/05-university-grade-and-enrollment-system/Main.java
+++ Programming-assignments/05-university-grade-and-enrollment-system/Main.java
@@ -3,16 +3,73 @@ public class Main {
 }
 
 
-class Student {
-    int id;
-    String name;
-    String[] enrolledCourses;
-
-    public Student(int id, String name, String[] enrolledCourses) {
-        this.id = id;
-        this.name = name;
-        this.enrolledCourses = enrolledCourses;
-    }
-    
+enum Message {
+    SUCCESS,
+    INDEX_OUT_OF_BOUNDS;
 }
 
+class Student {
+    private int id;
+    private String name;
+    private String[] enrolledCourses = new String[4];
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+        
+    }
+    
+    // getters and setters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String[] getEnrolledCourses() {
+        return enrolledCourses;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Message setCourse(int index, String courseName) {
+        if (index < 0 || index > 4) return Message.INDEX_OUT_OF_BOUNDS;
+    
+        enrolledCourses[index] = courseName;
+        return Message.SUCCESS;
+    }
+}
+
+class Course {
+    private int code;
+    private String name;
+    private int capacity;
+
+    public Course(int code, String name, int capacity) {
+        this.code = code;
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    public int getCode() {
+        return code;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
+
+class CourseManagement {
+    
+    
+}
