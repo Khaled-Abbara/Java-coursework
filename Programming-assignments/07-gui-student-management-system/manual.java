3731a6e9c8d133193b119e60ece1e206a53b10be
Khaled Abbara <148062226+Khaled-Abbara@users.noreply.github.com>
2025-10-21 19:34:26 +0800

    Submit

---
+++ Programming-assignments/07-gui-student-management-system/manual.java
@@ -0,0 +1,7 @@
+// button.addActionListener(event -> {
+// System.out.println("Button clicked!");
+// });
+
+// button.addActionListener(event -> {
+// JOptionPane.showMessageDialog(frame, "Hello there!");
+// });
