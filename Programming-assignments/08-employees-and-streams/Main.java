be5fb5a4b7ab790a7d727c66e8b60c2b882de20f
Khaled Abbara <148062226+Khaled-Abbara@users.noreply.github.com>
2025-10-31 09:25:08 +0800

    Submit

---
+++ Programming-assignments/08-employees-and-streams/Main.java
@@ -0,0 +1,49 @@
+
+// simple employee object
+class Employee {
+    private String name;
+    private int age;
+    private String department;
+    private double salary;
+
+    public Employee(String name, int age, String department, double salary) {
+        this.name = name;
+        this.age = age;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
